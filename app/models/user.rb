bfea6d5862d7449a5b6ad264d4760526a9129873
A-Mitsunaga <a56st70410@icloud.com>
2021-05-02 10:00:56 +0000

    [Add]4-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         has_many :post_images,dependent: :destroy
 end
 # 下記追記
 # 作成したUserモデルにdeviseで使用する機能が記述されている
