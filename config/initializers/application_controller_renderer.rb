3c59bfa9ea84c6624aef03fb514455446b188a5e
A-Mitsunaga <a56st70410@icloud.com>
2021-05-02 10:01:35 +0000

    [Add]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a2ea399b49deaf649fd0e0c127df1dcc0c5215f3c206bd17070e2a99e9043ce40ac1b877e6a45c11c391d56ec3948fd4d16170dc8dcf4db165de776c847acdc4'
